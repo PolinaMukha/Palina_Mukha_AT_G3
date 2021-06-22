e6bfa7f189dd13fc36bb00ad1bccdebe092e59a3
Palina Mukha <Palina_Mukha@epam.com>
2021-06-23 00:07:54 +0300

    String task - duplicated words

---
+++ src/main/java/tasks/day3/DuplicatedWords.java
@@ -0,0 +1,20 @@
+package main.java.tasks.day3;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class DuplicatedWords {
+
+    public static void DeleteDuplicates() {
+        String duplicatePattern = "(?i)\\b(\\w+)\\b[\\w\\W]*\\b\\1\\b";
+        Pattern p = Pattern.compile(duplicatePattern);
+        String phrase = "How much wood could a woodchuck chuck if a wooodchuck could chuck wood?" +
+                "A woodchuck could chuck as much wood as a woodchuck wouldchuck" +
+                "if a woodchuck could chuck wood.";
+        Matcher m = p.matcher(phrase);
+        while (m.find()) {
+            System.out.println("Duplicate word: " + m.group(1) + "\n");
+        }
+    }
+}
+
---
+++ src/main/java/tasks/day3/StringRunner.java
@@ -0,0 +1,9 @@
+package main.java.tasks.day3;
+
+public class StringRunner {
+
+    public static void main(String[] args) {
+
+        DuplicatedWords.DeleteDuplicates();
+    }
+}
