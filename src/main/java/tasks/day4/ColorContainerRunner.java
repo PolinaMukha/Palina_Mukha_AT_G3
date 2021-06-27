8053953e7ebb7bd60ef4dd183e63e39531dda6cf
Palina Mukha <Palina_Mukha@epam.com>
2021-06-27 12:07:33 +0300

    ColorConyainerColor

--- src/main/java/tasks/day4/ColorContainerRunner.java
+++ src/main/java/tasks/day4/ColorContainerRunner.java
@@ -2,17 +2,18 @@ package main.java.tasks.day4;
 
 public class ColorContainerRunner {
     public void printStringColor(String color) {
-        if (color.equals(ColorContainer.RED.getColor())){
+        if (color.equals(ColorContainer.RED.getColor())) {
             System.out.println(ColorContainer.RED.getColor());
-        }else if (){
-
-        } else{
-
+        } else if (color.equals(ColorContainer.BLUE.getColor())) {
+            System.out.println(ColorContainer.BLUE.getColor());
+        } else if (color.equals(ColorContainer.GREEN.getColor())) {
+            System.out.println(ColorContainer.GREEN.getColor());
+        } else if (color.equals(ColorContainer.YELLOW.getColor())) {
+            System.out.println(ColorContainer.YELLOW.getColor());
+        } else if (color.equals(ColorContainer.PURPLE.getColor())) {
+            System.out.println(ColorContainer.PURPLE.getColor());
         }
     }
-
-
     static private void printNumber(String color) {
-
     }
 }
