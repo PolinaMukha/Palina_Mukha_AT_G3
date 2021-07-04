d8387d240bd8f6e53dbb8cdb834e68d9eff36d05
Palina Mukha <Palina_Mukha@epam.com>
2021-07-04 16:20:58 +0300

    Collections - HashSet

--- src/main/java/tasks/day6/NewCollection.java
+++ src/main/java/tasks/day6/NewCollection.java
@@ -6,20 +6,20 @@ import java.util.List;
 public class NewCollection {
 
     public static void main(String[] args) {
-        List<String> myList= new ArrayList<>();
-        String str= "мама мыла раму";
+        List<String> myList = new ArrayList<>();
+        String str = "мама мыла раму мыла";
         String[] strNew = str.split(" ");
 
-        for (String s: strNew) {
+        for (String s : strNew) {
             System.out.println(s);
             myList.add(s);
         }
 
-        for (String sNew: myList) {
+        for (String sNew : myList) {
             System.out.println(sNew);
         }
 
-        for (int i = 0; i < myList.size(); i++){
+        for (int i = 0; i < myList.size(); i++) {
             System.out.println(myList.get(i));
         }
     }
