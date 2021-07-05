c17bfa73c59af741a6f1611ddb6733e55ad2dd57
Palina Mukha <Palina_Mukha@epam.com>
2021-07-06 01:16:01 +0300

    Collections - taskAuto

---
+++ src/main/java/tasks/day6/hometaskCollections/auto/Runner.java
@@ -0,0 +1,11 @@
+package main.java.tasks.day6.hometaskCollections.auto;
+
+import main.java.tasks.day6.hometaskCollections.figures.FigureFile;
+
+import java.io.IOException;
+
+public class Runner {
+    public static void main(String[] args) throws IOException {
+        AutoFile.write("C:/Repositories/Palina_Mukha_AT_G3/cars.txt");
+    }
+}
