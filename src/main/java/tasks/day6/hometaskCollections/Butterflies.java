45261b5e8a014c125ff37e0b22a7584c204cf419
Palina Mukha <Palina_Mukha@epam.com>
2021-07-05 23:41:23 +0300

    Collections - UpdateTaskButterflies

--- src/main/java/tasks/day6/hometaskCollections/Butterflies.java
+++ src/main/java/tasks/day6/hometaskCollections/Butterflies.java
@@ -2,6 +2,7 @@ package main.java.tasks.day6.hometaskCollections;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ListIterator;
 
 public class Butterflies {
     public static void main(String[] args) {
@@ -14,26 +15,23 @@ public class Butterflies {
         for (String b : butterflies) {
             System.out.println("'" + b + "'" + " ");
         }
+        System.out.println(butterflies);
 
         int counter = 0;
         for (String s : butterflies) {
             if (s.contains("o")) {
                 counter++;
             }
-
         }
-        System.out.println("----------");
         System.out.println(counter);
-        System.out.println("----------");
+
 
         for (int i = 0; i < butterflies.size(); i++) {
-            System.out.println(butterflies.get(i) + " ");
+            //System.out.println(butterflies.get(i) + " ");
         }
 
-        System.out.println("----------");
-
         for (String but : butterflies) {
-            System.out.println(but);
+            //System.out.println(but);
         }
     }
 }
