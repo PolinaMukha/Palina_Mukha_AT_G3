4d7e50c48b2d5ccd5db25fd9f33aed49f0c1331b
Palina Mukha <Palina_Mukha@epam.com>
2021-07-17 16:48:42 +0300

    day 8 - Threads

---
+++ src/main/java/tasks/day8/SyncRunner.java
@@ -0,0 +1,24 @@
+package main.java.tasks.day8;
+
+public class SyncRunner {
+
+    public static void main(String[] args) throws InterruptedException {
+        new Thread(() -> {
+            syncMethod("t1");
+        }).start();
+
+        Thread.sleep(1000);
+        syncMethod("m");
+    }
+
+    public static synchronized void syncMethod(String threadName) {
+        for (int i = 0; i < 3; i++) {
+            try {
+                Thread.currentThread().sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.printf("%s-%s ", threadName, i);
+        }
+    }
+}
